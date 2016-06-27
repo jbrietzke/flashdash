40fc16ce5dfd3918bee67635a9983c94d9637e4b
Barry Weinberg <bjw2119@gmail.com>
2016-06-27 18:09:34 -0400

    seeded?

--- Desktop/Bootcamp/week4/tripPlanner/trip/models/restaurant.js
+++ Desktop/Bootcamp/week4/tripPlanner/trip/models/restaurant.js
@@ -1,12 +1,12 @@
 var db = require('./index');
 var Place = require('./place');
 var Sequelize = require('sequelize');
-var Restaraunt = db.define('restaraunt', {
+var Restaurant = db.define('restaurant', {
   name : {type: Sequelize.STRING},
   cuisine :  {type: Sequelize.ARRAY(Sequelize.STRING)},
   price :  {type: Sequelize.INTEGER},
 });
 
-Restaraunt.belongsTo(Place);
+Restaurant.belongsTo(Place);
 
-module.exports = Restaraunt;
+module.exports = Restaurant;
