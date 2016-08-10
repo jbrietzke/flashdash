2b3dbf37cdc4ae15b5c38676550ef91d61fdac3a
Peter Lynn <github@fantutte.net>
2016-08-10 16:40:19 -0400

    Database name is now fdash

--- server/env/testing.js
+++ server/env/testing.js
@@ -1,6 +1,6 @@
 module.exports = {
-  "DATABASE_URI": "postgres://localhost:5432/testing-fsg",
-  "SESSION_SECRET": "Optimus Prime is my real dad",
+  "DATABASE_URI": "postgres://localhost:5432/testing-fdash",
+  "SESSION_SECRET": "Suspendisse sollicitudin odio diam, non.",
   "TWITTER": {
     "consumerKey": "INSERT_TWITTER_CONSUMER_KEY_HERE",
     "consumerSecret": "INSERT_TWITTER_CONSUMER_SECRET_HERE",
