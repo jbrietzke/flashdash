505f5f9f9c4436ace3a3ad6b952128e27ccb5990
Peter Lynn <github@fantutte.net>
2016-08-10 16:12:43 -0400

    Used  to stop on error

--- browser/js/docs/ticktest.controller.js
+++ browser/js/docs/ticktest.controller.js
@@ -11,12 +11,13 @@ app.factory('tickFactory', function($interval, $http) {
     let factory = {};
 
     factory.startTicking = function(Data) {
+        let ticker;
         function tick() {
             $http.get('/api/test/loghits')
             .then(rslt => { console.log("I ticked!"); Data.counter = rslt.data})
-            .catch(err => {clearInterval(ticker); Data.counter = "STOPPED"});
+            .catch(err => {$interval.cancel(ticker); Data.counter = "STOPPED"});
         }
-        let ticker = $interval(tick, 1000);
+        ticker = $interval(tick, 1000);
     }
 
     return factory;
