891f69f8761b21e1903f8353f876da6b05052248
Peter Lynn <github@fantutte.net>
2016-08-11 17:51:39 -0400

    Got the chart name dropdown

--- browser/js/dashboard/navbar/dashboard.navbar.js
+++ browser/js/dashboard/navbar/dashboard.navbar.js
@@ -1,39 +1,39 @@
 app.directive('dashboardnav', function ($rootScope, AuthService, AUTH_EVENTS, $state) {
-
-    return {
+/*
+running code before Compilation : use controller
+running code after Compilation : use Link
+Angular convention : write business logic in controller and DOM manipulation in link.
+*/
+ return {
         restrict: 'E',
         scope: {},
         templateUrl: 'js/dashboard/navbar/dashboard.navbar.html',
         link: function (scope) {
 
-
             scope.editable = false;
 
             scope.textEdit = 'Editing Disabled'
 
+            scope.dashboards = [
+                {name: "bar one", data: "trash"},
+                {name: "bar two", data: "trash"},
+                {name: "sports bar", data: "trash"},
+                {name: "simon bar sinister", data: "trash"},
+            ]
+            scope.item = scope.dashboards[0];
+            scope.update = function() {
 
+            };
             scope.toggleEdit = function(){
                 scope.editable = !scope.editable;
                 if(scope.editable){
                     scope.textEdit = 'Editing Enabled'
                     $rootScope.$broadcast('edit-enabled')
-
-                } else{
+                } else {
                     scope.textEdit = 'Editing Disabled'
                     $rootScope.$broadcast('edit-disabled')
                 }
-
-
-                //broad cast scope.editable to the dashboard controller
             }
-
-
-
-
-
-            // $rootScope.$on(AUTH_EVENTS.loginSuccess, setUser);
-            // $rootScope.$on(AUTH_EVENTS.logoutSuccess, removeUser);
-            // $rootScope.$on(AUTH_EVENTS.sessionTimeout, removeUser);
 
         }
 
