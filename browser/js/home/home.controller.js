b01f78a9b64fd2403a1368f6903e2e59eca12e34
Jackson Brietzke <jack.brietzke@gmail.com>
2016-08-11 12:57:17 -0400

    setting up dummy graphs to test gridster grid

--- browser/js/home/home.controller.js
+++ browser/js/home/home.controller.js
@@ -1,100 +1,256 @@
-app.controller('homeCtrl', function($scope, $timeout, DataService){
-  $scope.gridsterOptions = {
-    margins: [20, 20],
-    columns: 4,
-    mobileModeEnabled: false,
-    draggable: {
-      handle: 'h3'
-    },
-    resizable: {
-     enabled: true,
-     handles: ['n', 'e', 's', 'w', 'ne', 'se', 'sw', 'nw'],
+app.controller('homeCtrl', ['$scope', '$timeout', 'generator',
+    function($scope, $timeout, generator) {
+      $scope.gridsterOptions = {
+        margins: [20, 20],
+        columns: 4,
+        //mobileBreakPoint: 1000,
+        mobileModeEnabled: false,
+        draggable: {
+          handle: 'h3'
+        },
+        resizable: {
+          enabled: true,
+          handles: ['n', 'e', 's', 'w', 'ne', 'se', 'sw', 'nw'],
 
-     // optional callback fired when resize is started
-     start: function(event, $element, widget) {},
+          // optional callback fired when resize is started
+          start: function(event, $element, widget) {},
 
-     // optional callback fired when item is resized,
-     resize: function(event, $element, widget) {
-       if (widget.chart.api) widget.chart.api.update();
-     },
+          // optional callback fired when item is resized,
+          resize: function(event, $element, widget) {
+            if (widget.chart.api) widget.chart.api.update();
+          },
 
-      // optional callback fired when item is finished resizing
-     stop: function(event, $element, widget) {
-       $timeout(function(){
-         if (widget.chart.api) widget.chart.api.update();
-       },400)
-     }
-    },
-  };
+          // optional callback fired when item is finished resizing
+          stop: function(event, $element, widget) {
+            $timeout(function(){
+              if (widget.chart.api) widget.chart.api.update();
+            },400)
+          }
+        }
+      };
+      console.log(generator)
+      $scope.dashboard = {
+        widgets: [{
+          col: 0,
+          row: 0,
+          sizeY: 1,
+          sizeX: 2,
+          name: "Line Chart Widget",
+          type: 'lineChart',
+          chart: {
+            options: generator.lineChart.options(),
+            data: generator.lineChart.data(),
+            api: {}
+          }
+        }, {
+          col: 2,
+          row: 0,
+          sizeY: 1,
+          sizeX: 1,
+          name: "Pie Chart Widget",
+          type: 'pieChart',
+          chart: {
+            options: generator.pieChart.options(),
+            data: generator.pieChart.data(),
+            api: {}
+          }
+        }, {
+          col: 3,
+          row: 0,
+          sizeY: 1,
+          sizeX: 1,
+          name: "Box Plot Widget",
+          type: 'boxPlotChart',
+          chart: {
+            options: generator.boxPlotChart.options(),
+            data: generator.boxPlotChart.data(),
+            api: {}
+          }
+        }, {
+          col: 0,
+          row: 1,
+          sizeY: 1,
+          sizeX: 2,
+          name: "Discrete Bar Chart Widget",
+          type: 'discreteBarChart',
+          chart: {
+            options: generator.discreteBarChart.options(),
+            data: generator.discreteBarChart.data(),
+            api: {}
+          }
+        }, {
+          col: 2,
+          row: 1,
+          sizeY: 1,
+          sizeX: 2,
+          name: "Stacked Area Chart Widget",
+          type: 'stackedAreaChart',
+          chart: {
+            options: generator.stackedAreaChart.options(),
+            data: generator.stackedAreaChart.data(),
+            api: {}
+          }
+        }]
+      };
 
-  $scope.dashboard = {
-    widgets: [{
-      col: 0,
-      row: 0,
-      sizeY: 2,
-      sizeX: 2,
-      name: "Discrete Bar Chart",
-      chart: {
-        options: DataService.discreteBarChart.options(),
-        data: DataService.discreteBarChart.data(),
-        api: {}
-      }
-    }, {
-      col: 2,
-      row: 0,
-      sizeY: 2,
-      sizeX: 2,
-      name: "Candlestick Bar Chart",
-      chart: {
-        options: DataService.candlestickBarChart.options(),
-        data: DataService.candlestickBarChart.data(),
-        api: {}
-      }
-    }, {
-      col: 0,
-      row: 2,
-      sizeY: 2,
-      sizeX: 3,
-      name: "Line Chart",
-      chart: {
-        options: DataService.lineChart.options(),
-        data: DataService.lineChart.data(),
-        api: {}
-      }
-    }, {
-      col: 4,
-      row: 2,
-      sizeY: 1,
-      sizeX: 1,
-      name: "Pie Chart",
-      chart: {
-        options: DataService.pieChart.options(),
-        data: DataService.pieChart.data(),
-        api: {}
-      }
-    }]
-  };
+      // widget events
+      $scope.events = {
+        resize: function(e, scope){
+          $timeout(function(){
+            if (scope.api && scope.api.update) scope.api.update();
+          },200)
+        }
+      };
 
-  // We want to manually handle `window.resize` event in each directive.
-  // So that we emulate `resize` event using $broadcast method and internally subscribe to this event in each directive
-  // Define event handler
-  $scope.events = {
-    resize: function(e, scope){
+      $scope.config = { visible: false };
+
+      //make chart visible after grid have been created
       $timeout(function(){
-        scope.api.update()
-      },200)
-    }
-  };
-  angular.element(window).on('resize', function(e){
-    $scope.$broadcast('resize');
-  });
+        $scope.config.visible = true;
+      }, 200);
 
-  // We want to hide the charts until the grid will be created and all widths and heights will be defined.
-  // So that use `visible` property in config attribute
-  $scope.config = {
-    visible: false
-  };
-  $timeout(function(){
-    $scope.config.visible = true;
-  }, 200);
-});
+      //subscribe widget on window resize event
+      angular.element(window).on('resize', function(e){
+        $scope.$broadcast('resize');
+      });
+
+      // grid manipulation
+      $scope.clear = function() {
+        $scope.dashboard.widgets = [];
+      };
+
+      $scope.addWidget = function() {
+        $scope.dashboard.widgets.push({
+          name: "New Widget",
+          sizeX: 1,
+          sizeY: 1
+        });
+      };
+
+      //$scope.dashboards = {
+      //    '1': {
+      //        id: '1',
+      //        name: 'Home',
+      //        widgets: [{
+      //            col: 0,
+      //            row: 0,
+      //            sizeY: 1,
+      //            sizeX: 1,
+      //            name: "Widget 1"
+      //        }, {
+      //            col: 2,
+      //            row: 1,
+      //            sizeY: 1,
+      //            sizeX: 1,
+      //            name: "Widget 2"
+      //        }]
+      //    },
+      //    '2': {
+      //        id: '2',
+      //        name: 'Other',
+      //        widgets: [{
+      //            col: 1,
+      //            row: 1,
+      //            sizeY: 1,
+      //            sizeX: 2,
+      //            name: "Other Widget 1"
+      //        }, {
+      //            col: 1,
+      //            row: 3,
+      //            sizeY: 1,
+      //            sizeX: 1,
+      //            name: "Other Widget 2"
+      //        }]
+      //    }
+      //};
+      //$scope.$watch('selectedDashboardId', function(newVal, oldVal) {
+      //  if (newVal !== oldVal) {
+      //    $scope.dashboard = $scope.dashboards[newVal];
+      //  } else {
+      //    $scope.dashboard = $scope.dashboards[1];
+      //  }
+      //});
+
+      //// init dashboard
+      //$scope.selectedDashboardId = '1';
+    }
+  ])
+
+  .controller('CustomWidgetCtrl', ['$scope', '$modal',
+    function($scope, $modal) {
+
+      $scope.remove = function(widget) {
+        $scope.dashboard.widgets.splice($scope.dashboard.widgets.indexOf(widget), 1);
+      };
+
+      $scope.openSettings = function(widget) {
+        $modal.open({
+          scope: $scope,
+          templateUrl: 'pages/gridster/widgetSettings.html',
+          controller: 'WidgetSettingsCtrl',
+          resolve: {
+            widget: function() {
+              return widget;
+            }
+          }
+        });
+      };
+
+    }
+  ])
+
+  .controller('WidgetSettingsCtrl', ['$scope', '$timeout', '$rootScope', '$modalInstance', 'widget', 'generator',
+    function($scope, $timeout, $rootScope, $modalInstance, widget, generator) {
+      $scope.widget = widget;
+      $scope.widgetTypes = Object.keys(generator);
+
+      $scope.form = {
+        name: widget.name,
+        sizeX: widget.sizeX,
+        sizeY: widget.sizeY,
+        col: widget.col,
+        row: widget.row,
+        type: widget.type
+      };
+
+      $scope.sizeOptions = [{
+        id: '1',
+        name: '1'
+      }, {
+        id: '2',
+        name: '2'
+      }, {
+        id: '3',
+        name: '3'
+      }, {
+        id: '4',
+        name: '4'
+      }];
+
+      $scope.dismiss = function() {
+        $modalInstance.dismiss();
+      };
+
+      $scope.remove = function() {
+        $scope.dashboard.widgets.splice($scope.dashboard.widgets.indexOf(widget), 1);
+        $modalInstance.close();
+      };
+
+      $scope.submit = function() {
+        angular.extend(widget, $scope.form);
+
+        //update with new options and data
+        if (widget.type) {
+          widget.chart.options = generator[widget.type].options();
+          widget.chart.data = generator[widget.type].data();
+        }
+        $modalInstance.close(widget);
+
+        //update new chart
+        $timeout(function(){
+          widget.chart.api.update();
+        },600)
+      };
+
+    }
+  ])
