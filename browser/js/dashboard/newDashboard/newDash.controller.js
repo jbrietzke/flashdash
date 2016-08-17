3a792991fd68c2201211a4d1b03dfcb9a19ae030
mosane <ayush.singhvi@nyu.edu>
2016-08-17 17:18:37 -0400

    creating new dashboard creates modal, modal takes input thats reflected in the backend and redirects to the dash page

---
+++ browser/js/dashboard/newDashboard/newDash.controller.js
@@ -0,0 +1,15 @@
+app.controller('newDashboardCtrl', function (userFactory,$state,$uibModalInstance, $scope) {
+	
+	// $scope.form = {
+	// 	name: dashbaord.name, 
+	// 	description: dashboard.description
+	// }
+
+	$scope.submit = function () {
+	 	// angular.extend(dashboard, $scope.form)
+		return userFactory.addDashboard($scope.id, $scope.form)
+	    .then(function(){
+	    	$state.go('dashboard')
+	    })
+	}
+})
