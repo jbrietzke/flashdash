c94dafcdffda6ca28c901f13cecdbb832869f49b
Jackson Brietzke <jack.brietzke@gmail.com>
2016-08-19 15:18:15 -0400

    to jackson adding DB uri

--- server/env/development.js
+++ server/env/development.js
@@ -1,5 +1,6 @@
 module.exports = {
-  "DATABASE_URI": "postgres://localhost:5432/fdash",
+  "DATABASE_URI": "postgres://cbiopidvcymizk:_PqfUxNxFKkAjyfPqR-M6i8tvz@ec2-54-227-245-222.compute-1.amazonaws.com:5432/d3trq16gajaq2o",
+  "DATABASE_URL": "postgres://cbiopidvcymizk:_PqfUxNxFKkAjyfPqR-M6i8tvz@ec2-54-227-245-222.compute-1.amazonaws.com:5432/d3trq16gajaq2o",
   "SESSION_SECRET": "Optimus Prime is my real dad",
   "TWITTER": {
     "consumerKey": "INSERT_TWITTER_CONSUMER_KEY_HERE",
