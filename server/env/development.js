ee9f31b89ea240c0afa69e36b1727ef97ffa6cbc
Jackson Brietzke <jack.brietzke@gmail.com>
2016-08-19 15:55:01 -0400

    to JAck

--- server/env/development.js
+++ server/env/development.js
@@ -1,6 +1,5 @@
 module.exports = {
-  "DATABASE_URI": "postgres://cbiopidvcymizk:_PqfUxNxFKkAjyfPqR-M6i8tvz@ec2-54-227-245-222.compute-1.amazonaws.com:5432/d3trq16gajaq2o",
-  "DATABASE_URL": "postgres://cbiopidvcymizk:_PqfUxNxFKkAjyfPqR-M6i8tvz@ec2-54-227-245-222.compute-1.amazonaws.com:5432/d3trq16gajaq2o",
+  "DATABASE_URI": "postgres://localhost:5432/fdash",
   "SESSION_SECRET": "Optimus Prime is my real dad",
   "TWITTER": {
     "consumerKey": "INSERT_TWITTER_CONSUMER_KEY_HERE",
