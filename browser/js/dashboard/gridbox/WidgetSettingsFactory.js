3b023d64ea733f62b2dd51a99395b8dc7bc5fed3
ray11furtado <rayfurtado11@gmail.com>
2016-08-22 13:01:12 -0400

    Update WidgetSettingsFactory.js

--- browser/js/dashboard/gridbox/WidgetSettingsFactory.js
+++ browser/js/dashboard/gridbox/WidgetSettingsFactory.js
@@ -10,16 +10,5 @@ app.factory('WidgetSettingsFactory', function(DashboardFactory){
       })
   }
 
-//   factory.setDataInKVFormat = function (realData) {
-//     return [{
-//       values: realData,
-//       key: null 
-//       // this breaks previously hardcoded 'this works' string in the tooltip
-//       // for key - the key should be the yparameter as 
-//       // sepecified by the user - the new graph form or widget settings form 
-//       // need to be modified to accomodate this
-//       // also breaks color which was hardcoded - should also be user input
-//     }]
-//   }
   return factory;
 })
